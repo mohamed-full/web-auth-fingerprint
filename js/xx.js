b19ef416e403b5d3e4ec3c99070dca132d8dbe04
Mohammed <dxooxds@gmail.com>
2022-08-17 15:56:05 +0300

    first upload

---
+++ js/xx.js
@@ -0,0 +1,26 @@
+   
+
+                
+    var randomChallengeBuffer = new Uint8Array(32);
+    window.crypto.getRandomValues(randomChallengeBuffer);
+
+    var base64id = 'MIIBkzCCATigAwIBAjCCAZMwggE4oAMCAQIwggGTMII='
+    var idBuffer = Uint8Array.from(window.atob(base64id), c=>c.charCodeAt(0))
+
+    var publicKey = {
+        challenge: randomChallengeBuffer,
+
+        rp: { name: "protocol FIDO" },
+
+        user: {
+            id: idBuffer,
+            name: "phone@example.com",
+            displayName: "phone"
+        },
+
+        attestation: 'direct',
+        pubKeyCredParams: [
+            { type: 'public-key',  alg: -7  },
+            { type: 'public-key', alg: -257 } 
+        ]
+    }
